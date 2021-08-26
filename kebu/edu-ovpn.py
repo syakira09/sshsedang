538f719f126de44858354eac4a027683f5335f27
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-26 23:44:30 +0700

    Update edu-ovpn.py

--- kebu/edu-ovpn.py
+++ kebu/edu-ovpn.py
@@ -11,7 +11,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:1194'
-RESPONSE = 'HTTP/1.1 101 <i><u><font color="#00FFFF">Setup By SSH SEDANG</font></u>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
+RESPONSE = 'HTTP/1.1 101 <i><u><font color="red">Setup By SSH SEDANG</font></u>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):
