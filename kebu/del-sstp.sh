3ad9c39d348696634da368e18439cb1044242d80
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-26 20:36:07 +0700

    Update del-sstp.sh

--- kebu/del-sstp.sh
+++ kebu/del-sstp.sh
@@ -4,7 +4,7 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ifconfig.me/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/gaspoll/main/ipvps | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kota/ipvps?token=AVIBESYXEVQJ22UELRCJTQLBE6CJS | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
@@ -16,19 +16,19 @@ clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/premium-script/data-user-sstp")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
-		echo "Name : Delete SSTP Account"
-		echo ""
-		echo "You have no existing clients!"
+		echo "Name : Delete SSTP Account" | lolcat
+		echo "=============================="
+		echo "You have no existing clients!" | lolcat
 		exit 1
 	fi
 
 	echo ""
 	echo " Name : Delete SSTP Account"
-	echo ""
+	echo "==============================" | lolcat
 	echo " Select the existing client you want to remove"
 	echo " Press CTRL+C to return"
 	echo ""
-	echo " ==============================="
+	echo " ===============================" | lolcat
 	echo "     No  Expired   User"
 	grep -E "^### " "/var/lib/premium-script/data-user-sstp" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
@@ -44,7 +44,7 @@ exp=$(grep -E "^###" /var/lib/premium-script/data-user-sstp | cut -d ' ' -f 3 | 
 sed -i "/^### $user $exp/d" /var/lib/premium-script/data-user-sstp
 sed -i '/^'"$user"'/d' /home/sstp/sstp_account
 clear
-echo " SSTP Account Has Been Successfully Deleted"
+echo " SSTP Account Has Been Successfully Deleted" | lolcat
 echo " =========================="
 echo " Client Name : $user"
 echo " Expired On  : $exp"
