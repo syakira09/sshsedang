f7982c0da1a6c2a9f4dc8fa8eb553a84ab717f08
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-26 22:30:45 +0700

    Update cff.sh

--- kebu/cff.sh
+++ kebu/cff.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /root
-echo "Name : Add ID Cloudlare "
+echo "Name : Add ID Cloudlare " | lolcat
 echo ""
 read -e -p " Masukan Domain :$domain" domain
 read -e -p " Masukan Email Cloudflare :" email
@@ -12,8 +12,8 @@ echo -e "key=$key" >> /root/mail.conf
 clear
 echo "Done"
 echo "Your ID Cloudflare"
-echo -e "==============================="
+echo -e "===============================" | lolcat
 echo "DOMAIN         : $domain"
 echo "Email          : $email"
 echo "Api Key        : $key"
-echo -e "==============================="
+echo -e "===============================" | lolcat
