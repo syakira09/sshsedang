bd87524d932fd6e7810eb8a81758f89d76560bcb
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-26 22:50:52 +0700

    Update renew-wg.sh

--- kebu/renew-wg.sh
+++ kebu/renew-wg.sh
@@ -4,7 +4,7 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ifconfig.me/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kota/ipvps?token=AVIBESYXEVQJ22UELRCJTQLBE6CJS | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kota/ipvps | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
@@ -54,7 +54,7 @@ sed -i "s/### Client $user $exp/### Client $user $exp4/g" /etc/wireguard/wg0.con
 clear
 echo ""
 echo " Wireguard Account Has Been Successfully Renewed"
-echo " =========================="
+echo " ==========================" | lolcat
 echo " Client Name : $user"
 echo " Expired  On: $exp4"
-echo " =========================="
+echo " ==========================" | lolcat
