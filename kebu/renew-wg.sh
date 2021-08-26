cabefa568d12eb6028edc2b867b9cadac914af4a
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-26 20:13:30 +0700

    Update renew-wg.sh

--- kebu/renew-wg.sh
+++ kebu/renew-wg.sh
@@ -4,7 +4,7 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ifconfig.me/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/gaspoll/main/ipvps | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kota/ipvps?token=AVIBESYXEVQJ22UELRCJTQLBE6CJS | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
@@ -18,7 +18,7 @@ source /etc/wireguard/params
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		clear
 		echo ""
-		echo "Name : Renew Wireguard Account"		
+		echo "Name : Renew Wireguard Account" | lolcat	
 		echo ""		
 		echo "You have no existing clients!"
 		exit 1
@@ -26,7 +26,7 @@ source /etc/wireguard/params
 
 	clear
 	echo ""
-	echo "Name : Renew Wireguard Account"		
+	echo "Name : Renew Wireguard Account" | lolcat	
 	echo ""		
 	echo "Select an existing client that you want to renew"
 	echo " Press CTRL+C to return"
