97e953c631b124b613507a1232e3490f689654c0
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-26 23:34:34 +0700

    Update cek-vless.sh

--- kebu/cek-vless.sh
+++ kebu/cek-vless.sh
@@ -4,7 +4,7 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ifconfig.me/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/gaspoll/main/ipvps | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kota/ipvps | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
@@ -15,7 +15,7 @@ fi
 echo -n > /tmp/other.txt
 data=( `cat /etc/v2ray/vless.json | grep '^###' | cut -d ' ' -f 2`);
 echo "-------------------------------";
-echo "Name : Vless User Login";
+echo "Name : Vless User Login"; | lolcat
 echo "-------------------------------";
 for akun in "${data[@]}"
 do
