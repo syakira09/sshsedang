0a101f99cf23b192d8d13539eb81b028ddd2892e
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-27 13:36:23 +0700

    Update del-sstp.sh

--- kebu/del-sstp.sh
+++ kebu/del-sstp.sh
@@ -4,7 +4,7 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ifconfig.me/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kota/ipvps?token=AVIBESYXEVQJ22UELRCJTQLBE6CJS | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kota/ipvps | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
