bc544f932febeedf811f0412c9eded0adbef0c5f
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-27 17:44:59 +0700

    Update menu.sh

--- men/menu.sh
+++ men/menu.sh
@@ -82,7 +82,7 @@ echo -e   "  \e[1;32m╠══════════════════�
 echo -e   "  ║ x)   Exit                                                  ║\e[m" | lolcat
 echo -e   "  \e[1;32m╚════════════════════════════════════════════════════════════╝\e[m" | lolcat
 echo -e   ""
-read -p "     Select From Options [1-30 or x] :  " menu
+read -p "     Select From Options [1-32 or x] :  " menu
 echo -e   ""
 case $menu in
 1)
