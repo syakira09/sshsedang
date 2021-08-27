df85cc246ae7245497df3a87c0fb21a647fd67c5
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-27 13:35:53 +0700

    Update add-sstp.sh

--- kebu/add-sstp.sh
+++ kebu/add-sstp.sh
@@ -4,7 +4,7 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ifconfig.me/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kota/ipvps?token=AVIBESYXEVQJ22UELRCJTQLBE6CJS | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kota/ipvps| grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
@@ -36,8 +36,9 @@ EOF
 echo -e "### $user $exp">>"/var/lib/premium-script/data-user-sstp"
 clear
 cat <<EOF
-Name : SSTP VPN
+
 echo -e "============================" | lolcat
+Name : SSTP VPN
 
 Server IP     : $IP
 Username      : $user
