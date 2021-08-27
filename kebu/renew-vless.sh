766ef0649b0a56b43546163f94c5e8b92f653d17
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-27 19:52:10 +0700

    Update renew-vless.sh

--- kebu/renew-vless.sh
+++ kebu/renew-vless.sh
@@ -4,33 +4,21 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ifconfig.me/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kota/ipvps | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-exit 0
-fi
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/v2ray/vless.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		clear
-		echo ""
-		echo "Name :  Renew Vless Account" | lolcat
-		echo ""
-		echo "You have no existing clients!" 
+		echo "Name : Renew Vless Account"
+		echo "===============================" | lolcat
+		echo "You have no existing clients!"
 		exit 1
 	fi
 
 	clear
 	echo ""
-	echo "Name :  Renew Vless Account" | lolcat
-	echo ""
 	echo "Select the existing client you want to renew"
 	echo " Press CTRL+C to return"
-	echo ""
-	echo -e "===============================" | lolcat
+	echo -e "==============================="
 	grep -E "^### " "/etc/v2ray/vless.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
