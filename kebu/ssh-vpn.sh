7ae628e3fe4bbc0e9067c9cf22965866ba46ba04
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-27 16:53:45 +0700

    Update ssh-vpn.sh

--- kebu/ssh-vpn.sh
+++ kebu/ssh-vpn.sh
@@ -34,7 +34,7 @@ chmod +x /usr/local/bin/edu-ovpn
 cat > /etc/systemd/system/edu-ovpn.service << END
 [Unit]
 Description=Python Edu Ovpn By Allah
-Documentation=https://sosohoha.my.id
+Documentation=https://sshsedang.my.id
 After=network.target nss-lookup.target
 
 [Service]
@@ -61,8 +61,8 @@ chmod +x /usr/local/bin/ws-dropbear
 # Installing Service
 cat > /etc/systemd/system/ws-dropbear.service << END
 [Unit]
-Description=Python Edu Ovpn By Allah
-Documentation=https://sosohoha.my.id
+Description=Python Edu Ovpn By SSH SEDANG
+Documentation=https://sshsedang.my.id
 After=network.target nss-lookup.target
 
 [Service]
@@ -168,7 +168,7 @@ sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git lsof
 echo "clear" >> .profile
 echo "neofetch" >> .profile
-echo "echo Mod By Sukirman" >> .profile
+echo "echo Script Premium By SSH Sedang Network" >> .profile
 echo "echo Silahkan Ketik Menu Untuk Melihat daftar Perintah" >> .profile
 
 # install webserver
