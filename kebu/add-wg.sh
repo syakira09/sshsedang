2666754736a33b604d59e416ff7df683016cf5d0
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-28 08:13:05 +0700

    Update add-wg.sh

--- kebu/add-wg.sh
+++ kebu/add-wg.sh
@@ -94,4 +94,6 @@ AllowedIPs = $CLIENT_ADDRESS/32" >>"/etc/wireguard/$SERVER_WG_NIC.conf"
 	echo -e "Wireguard	: http://$MYIP:81/$CLIENT_NAME.conf"
 	echo -e "==============================="
 	echo -e "Expired On      : $exp"
+	echo -e "==============================="
+	echo -e "Script By SSH SEDANG NETWORK"
 	rm -f /root/wg0-client-$CLIENT_NAME.conf
