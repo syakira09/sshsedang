573eb57609b3e3c07ac71368eb32a40e7fd79bba
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-28 09:20:25 +0700

    Update autokill.sh

--- kebu/autokill.sh
+++ kebu/autokill.sh
@@ -16,7 +16,7 @@ sts="${Error}"
 fi
 clear
 echo -e ""
-echo -e "======================================"
+echo -e "======================================" | lolcat
 echo -e ""
 echo -e "     Status Autokill $sts        "
 echo -e ""
@@ -25,7 +25,7 @@ echo -e "     [2]  AutoKill After 10 Minutes"
 echo -e "     [3]  AutoKill After 15 Minutes"
 echo -e "     [4]  Turn Off AutoKill/MultiLogin"
 echo -e "     [x]  Exit"
-echo -e "======================================"                                                                                                          
+echo -e "======================================" | lolcat                                                                                                          
 echo -e ""
 read -p "     Select From Options [1-4 or x] :  " AutoKill
 read -p "     Multilogin Maximum Number Of Allowed: " max
@@ -39,12 +39,12 @@ case $AutoKill in
                 echo "# Autokill" >>/etc/cron.d/tendang
                 echo "*/5 * * * *  root /usr/bin/tendang $max" >>/etc/cron.d/tendang
                 echo -e ""
-                echo -e "======================================"
+                echo -e "======================================" | lolcat
                 echo -e ""
                 echo -e "      Allowed MultiLogin : $max"
                 echo -e "      AutoKill Every     : 5 Minutes"      
                 echo -e ""
-                echo -e "======================================"                                                                                                                                 
+                echo -e "======================================" | lolcat                                                                                                                                 
                 exit                                                                  
                 ;;
                 2)
@@ -55,12 +55,12 @@ case $AutoKill in
                 echo "# Autokill" >>/etc/cron.d/tendang
                 echo "*/10 * * * *  root /usr/bin/tendang $max" >>/etc/cron.d/tendang
                 echo -e ""
-                echo -e "======================================"
+                echo -e "======================================" | lolcat
                 echo -e ""
                 echo -e "      Allowed MultiLogin : $max"
                 echo -e "      AutoKill Every     : 10 Minutes"
                 echo -e ""
-                echo -e "======================================"
+                echo -e "======================================" | lolcat
                 exit
                 ;;
                 3)
@@ -71,23 +71,23 @@ case $AutoKill in
                 echo "# Autokill" >>/etc/cron.d/tendang
                 echo "*/15 * * * *  root /usr/bin/tendang $max" >>/etc/cron.d/tendang
                 echo -e ""
-                echo -e "======================================"
+                echo -e "======================================" | lolcat
                 echo -e ""
                 echo -e "      Allowed MultiLogin : $max"
                 echo -e "      AutoKill Every     : 15 Minutes"
                 echo -e ""
-                echo -e "======================================"
+                echo -e "======================================" | lolcat
                 exit
                 ;;
                 4)
                 clear
                 echo > /etc/cron.d/tendang
                 echo -e ""
-                echo -e "======================================"
+                echo -e "======================================" | lolcat
                 echo -e ""
                 echo -e "      AutoKill MultiLogin Turned Off  "
                 echo -e ""
-                echo -e "======================================"
+                echo -e "======================================" | lolcat
                 exit
                 ;;
                 x)
