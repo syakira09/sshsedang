c2185872b2dc2295e6901b5e23ab77c906e7dfe5
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-29 06:53:54 +0700

    Update about.sh

--- kebu/about.sh
+++ kebu/about.sh
@@ -4,22 +4,22 @@ clear
 echo -e "=================================================" | lolcat
 echo -e "#     Premium Auto Script By SSH Sedang         #" | lolcat
 echo -e "#-----------------------------------------------#" | lolcat
-echo -e "# For Debian 9 & Debian 10 64 bit               #" | lolcat
-echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #" | lolcat
-echo -e "# For VPS with KVM and VMWare virtualization    #" | lolcat
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #" 
+echo -e "# For VPS with KVM and VMWare virtualization    #" 
 echo -e "#-----------------------------------------------#" | lolcat
 echo -e "# Thanks To  :                                  #" | lolcat
 echo -e "#-----------------------------------------------#" | lolcat
-echo -e "# ALLAH S.W.T                                   #" | lolcat
-echo -e "# My Family                                     #" | lolcat
-echo -e "# Google                                        #" | lolcat
-echo -e "# Admin Portal & White Vps                      #" | lolcat
-echo -e "# Tuan YZ                                       #" | lolcat
-echo -e "# Adisubagja                                    #" | lolcat
-echo -e "# Demonk                                        #" | lolcat
-echo -e "#                                               #" | lolcat
+echo -e "# ALLAH S.W.T                                   #" 
+echo -e "# My Family                                     #" 
+echo -e "# Google                                        #" 
+echo -e "# Admin Portal & White Vps                      #" 
+echo -e "# Tuan YZ                                       #" 
+echo -e "# Adisubagja                                    #"
+echo -e "# Demonk                                        #" 
+echo -e "#                                               #" 
 echo -e "#-----------------------------------------------#" | lolcat
-echo -e "# Rebuild By : SSH SEDANG NETWORK               #" | lolcat
-echo -e "# Contact    : t.me/sshsedang4                  #" | lolcat
-echo -e "# Whatsapp   : 082311190332                     #" | lolcat
+echo -e "# Rebuild By : SSH SEDANG NETWORK               #" 
+echo -e "# Contact    : t.me/sshsedang4                  #" 
+echo -e "# Whatsapp   : 082311190332                     #" 
 echo -e "=================================================" | lolcat
