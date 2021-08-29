a1988af87055614cfa78f5fe3fd5658b8c0a8fac
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-29 10:21:40 +0700

    Update renew-tr.sh

--- kebu/renew-tr.sh
+++ kebu/renew-tr.sh
@@ -17,19 +17,18 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/trojan/akun.conf")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		clear
 		echo ""
-		echo "Name : Renew Trojan Account" | lolcat
-		echo ""
+		echo "Name : Renew Trojan Account" 
+		echo "============================" | lolcat
 		echo "You have no existing clients!"
 		exit 1
 	fi
 
 	clear
 	echo ""
-	echo "Name : Renew Trojan Account" | lolcat
-	echo ""
+	echo "Name : Renew Trojan Account" 
+	echo "===============================" | lolcat
 	echo "Select the existing client you want to renew"
-	echo " Press CTRL+C to return"
-	echo ""
+	echo "Press CTRL+C to return"
 	echo -e "===============================" | lolcat
 	grep -E "^### " "/etc/trojan/akun.conf" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
@@ -52,7 +51,8 @@ sed -i "s/### $user $exp/### $user $exp4/g" /etc/trojan/akun.conf
 clear
 echo ""
 echo " Trojan Account Was Successfully Renewed"
-echo " ==========================" | lolcat
+echo " ============================" | lolcat
 echo " Client Name : $user"
 echo " Expired On  : $exp4"
-echo " ==========================" | lolcat
+echo " ============================" | lolcat
+echo " Script By SSH SEDANG NETWORK"
