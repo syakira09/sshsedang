eeed5289db44468285be426b16e99ca8c95d9779
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-29 09:36:51 +0700

    Update usernew.sh

--- kebu/usernew.sh
+++ kebu/usernew.sh
@@ -65,11 +65,11 @@ echo -e "WS Ovpn       : 2086"
 echo -e "Port Squid    :$sqd"
 echo -e "badvpn        : 7100-7300"
 echo -e "===============================" | lolcat
-echo -e "OpenVPN       : TCP $ovpn http://$IP:81/client-tcp-$ovpn.ovpn"
-echo -e "OpenVPN       : UDP $ovpn2 http://$IP:81/client-udp-$ovpn2.ovpn"
-echo -e "OpenVPN       : SSL 442 http://$IP:81/client-tcp-ssl.ovpn"
+echo -e "OpenVPN TCP   : $ovpn http://$IP:81/client-tcp-$ovpn.ovpn"
+echo -e "OpenVPN UDP   : $ovpn2 http://$IP:81/client-udp-$ovpn2.ovpn"
+echo -e "OpenVPN SSL   : 442 http://$IP:81/client-tcp-ssl.ovpn"
 echo -e "===============================" | lolcat
 echo -e "[PAYLOAD SSH WS]"
 echo -e "GET / HTTP/1.1[crlf]Host: $domain[crlf]Upgrade: websocket[crlf][crlf]"
 echo -e "===============================" | lolcat
-echo -e "SCRIPT By SSH SEDANG"
+echo -e "Script By SSH SEDANG NETWORK"
