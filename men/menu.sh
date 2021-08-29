483cf0760864ee364aed49e928a30c586bb75e99
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-29 13:36:41 +0700

    Update menu.sh

--- men/menu.sh
+++ men/menu.sh
@@ -34,7 +34,6 @@ IPVPS=$(curl -s ipinfo.io/ip )
 	tmemo=$( df -h | awk 'FNR == 4 {print $4}' )
 	swap=$( free -m | awk 'NR==4 {print $2}' )
 	up=$(uptime|awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=""; print }')
-	
 	echo -e " $bl ║ \e[032;1mCPU Model      :\e[0m$bd $cname  "
 	echo -e " $bl ║ \e[032;1mNumber Of Cores        :\e[0m$bd $cores"
 	echo -e " $bl ║ \e[032;1mCPU Frequency          :\e[0m$bd $freq MHz"
