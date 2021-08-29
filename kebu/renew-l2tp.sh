2dfc3e097575a2a610fb0e2df236a5cd96d15f10
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-29 10:14:06 +0700

    Update renew-l2tp.sh

--- kebu/renew-l2tp.sh
+++ kebu/renew-l2tp.sh
@@ -17,19 +17,18 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/premium-script/data-user-l2tp")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		clear
 		echo ""
-		echo "Name :  Renew L2TP" | lolcat
-		echo ""
+		echo "Name :  Renew L2TP Account" 
+		echo "==============================" | lolcat
 		echo "You have no existing clients!"
 		exit 1
 	fi
 
 	clear
 	echo ""
-	echo "Name :  Renew L2TP" | lolcat
-	echo ""
+	echo "Name :  Renew L2TP Account" 
+	echo "===============================" | lolcat
 	echo "Select the existing client you want to renew"
-	echo " Press CTRL+C to return"
-	echo ""
+	echo "Press CTRL+C to return"
 	echo -e "===============================" | lolcat
 	grep -E "^### " "/var/lib/premium-script/data-user-l2tp" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
@@ -52,7 +51,8 @@ sed -i "s/### $user $exp/### $user $exp4/g" /var/lib/premium-script/data-user-l2
 clear
 echo ""
 echo " L2TP Account Has Been Successfully Renewed"
-echo " ==========================" | lolcat
+echo " ============================" | lolcat
 echo " Client Name : $user"
 echo " Expired On  : $exp4"
-echo " ==========================" | lolcat
+echo " ============================" | lolcat
+echo " Script By SSH SEDANG NETWORK"
