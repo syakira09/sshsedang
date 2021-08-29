79966f20fca34eb3d5898b8bfd6823ca1b9f7cf3
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-29 19:32:13 +0700

    Update setup.sh

--- kota/setup.sh
+++ kota/setup.sh
@@ -43,7 +43,7 @@ wget https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kebu/ins-vt.sh 
 wget https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kebu/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
 wget https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kebu/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 #install trojan-go
-wget https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/men/ins-trojango.sh && dos2unix ins-trojango.sh && chmod +x ins-trojango.sh && ./ins-trojango.sh
+#wget https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/men/ins-trojango.sh && dos2unix ins-trojango.sh && chmod +x ins-trojango.sh && ./ins-trojango.sh
 
 rm -f /root/ssh-vpn.sh
 rm -f /root/sstp.sh
@@ -53,7 +53,7 @@ rm -f /root/wg.sh
 rm -f /root/ins-vt.sh
 rm -f /root/ipsec.sh
 rm -f /root/set-br.sh
-rm -f /root/ins-trojango.sh
+#rm -f /root/ins-trojango.sh
 
 cat <<EOF> /etc/systemd/system/autosett.service
 [Unit]
@@ -103,7 +103,7 @@ echo "   - V2RAY Vmess None TLS    : 80"  | tee -a log-install.txt
 echo "   - V2RAY Vless TLS         : 2083"  | tee -a log-install.txt
 echo "   - V2RAY Vless None TLS    : 8880"  | tee -a log-install.txt
 echo "   - Trojan                  : 2087"  | tee -a log-install.txt
-echo "   - TrojanGO                : 2096"  | tee -a log-install.txt
+#echo "   - TrojanGO                : 2096"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
 echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
